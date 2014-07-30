958e80578ae2c62a7b19e4ff45510171e9f613ef
cloneko <cloneko@gmail.com>
2014-07-30 15:19:15 +0900

    Add testcode

---
+++ _test/validator.py
@@ -0,0 +1,16 @@
+import pytest
+import json
+from py_w3c.validators.html.validator import HTMLValidator
+
+validator = HTMLValidator()
+validator.validate_file('../index.html')
+
+def test_has_no_error():
+	assert len(validator.errors) == 0
+
+def test_has_no_warning():
+	assert len(validator.warnings) == 0
+
+
+
+
